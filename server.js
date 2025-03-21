82515d93993bfc2c0d851986d199a6bfc6576ac6
Pavlo <pavlokostereva@gmail.com>
2025-03-21 15:48:30 +0200

    express.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const http = require('http');//завантаження вбудованого модуля. Це для створення веб-сервера
-const hostname = '127.0.0.1'// сервер буде працювати на локальному комп'ютері
-const port = 3000;//сервер буде лслухати http- запити на порту 3000
+const express = require('express');  
+const app = express();
 
-const server = http.createServer((req, res) => {//створюємо запит  і (req, res)- колбек функція,
-    //яка викликається кожного разу коли отримує http-запит
-
-    res.statusCode = 200;//статус відповіді 
-    res.setHeader('Content-Type', 'text/plain');//content-type повідомляє веб-браузеру, що відповідь містить текст
-    res.end("Hello, Node.js");
-
+// Оголошення маршруту  
+app.get('/', (req, res) => {  
+ res.send('Hello, World!');  
 });
 
-server.listen(port, hostname, () => {//запуск сервера, щоб він слухав підключення на hostname:port
-    console.log(`The server is running on http://${hostname}:${port}/`);// виведе після запуску The server is running on http://127.0.0.1:3000/
-
+// Запуск сервера  
+const PORT = process.env.PORT || 3000;  
+app.listen(PORT, () => {  
+ console.log(`Server is running on http://localhost:${PORT}`);  
 });
